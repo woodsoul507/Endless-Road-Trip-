392e9632eb31419116eed8cd8a35d56641858345
Jose Alfu <josealfu@gmail.com>
2022-04-12 05:37:48 -0500

    pointer controllers added using ui canvas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  public SpawnManager spawnManager;
-  void Start()
-  {
+  [SerializeField] float sideSpeed = 8f;
+  [SerializeField] float forwardSpeed = 40f;
+  [SerializeField] SpawnManager spawnManager;
 
-  }
+  bool moveLeft;
+  bool moveRight;
 
   void Update()
   {
+    transform.position += Vector3.forward * forwardSpeed * Time.deltaTime;
 
+    if (moveLeft)
+    {
+      transform.position += Vector3.left * sideSpeed * Time.deltaTime;
+    }
+
+    if (moveRight)
+    {
+      transform.position += Vector3.right * sideSpeed * Time.deltaTime;
+    }
   }
 
   void OnTriggerExit(Collider other)
@@ -21,5 +32,25 @@ public class PlayerController : MonoBehaviour
     {
       spawnManager.SpawnTriggerExit();
     }
+  }
+
+  public void MoveLeftOn()
+  {
+    moveLeft = true;
+  }
+
+  public void MoveLeftOff()
+  {
+    moveLeft = false;
+  }
+
+  public void MoveRightOn()
+  {
+    moveRight = true;
+  }
+
+  public void MoveRightOff()
+  {
+    moveRight = false;
   }
 }
